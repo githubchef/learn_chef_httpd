b616d44dfbcb49e6b215688ff52d1ee76fe8d862
Github User <githubchef@gmail.com>
2017-11-06 03:28:43 -0700

    Added Httpd

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package httpd do
+  action :install
+end
+
+service httpd do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  mode '0644'
+end
